ea03f6d46ddba8d07569657d7818c29f4f256d6e
DeJw92 <dpawlak1992@gmail.com>
2014-12-26 20:40:32 +0100

    adding possibility to enable and disable bluetooth connection

--- src/com/example/RC_Car/MyActivity.java
+++ src/com/example/RC_Car/MyActivity.java
@@ -2,6 +2,7 @@ package com.example.RC_Car;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 
 public class MyActivity extends Activity {
 
+    private static final int REQUEST_ENABLE_BT = 1;
 
     private Button connectButton;
     private Button disconnectButton;
@@ -31,22 +33,26 @@ public class MyActivity extends Activity {
     private void setUp() {
         initializeComponents();
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        setBluetoothStatus();
-    }
-
-    private void setBluetoothStatus() {
         if(bluetoothAdapter == null) {
             textView.setText(R.string.bluetooth_connection_error);
             finish();
         }
+        setBluetoothStatus();
+    }
+
+    private void setBluetoothStatus() {
         if(bluetoothAdapter.isEnabled()) {
             textView.setText(R.string.bluetooth_connection_on);
             connectButton.setVisibility(View.GONE);
+            disconnectButton.setVisibility(View.VISIBLE);
+            showPairedDevicesButton.setVisibility(View.VISIBLE);
+            searchNewDevicesButton.setVisibility(View.VISIBLE);
         } else {
             textView.setText(R.string.bluetooth_connection_off);
             disconnectButton.setVisibility(View.GONE);
             showPairedDevicesButton.setVisibility(View.GONE);
             searchNewDevicesButton.setVisibility(View.GONE);
+            connectButton.setVisibility(View.VISIBLE);
         }
     }
 
@@ -59,11 +65,19 @@ public class MyActivity extends Activity {
     }
 
     public void connectButtonHandler(View view) {
+        Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBluetooth, REQUEST_ENABLE_BT);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        setBluetoothStatus();
     }
 
     public void disconnectButtonHandler(View view) {
-
+        bluetoothAdapter.disable();
+        setBluetoothStatus();
     }
 
     public void showPairedDevicesButtonHandler(View view) {
