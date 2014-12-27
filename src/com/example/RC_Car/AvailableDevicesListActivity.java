5659ce2e72bba9063e399ed6ebe8e8281629b616
DeJw92 <dpawlak1992@gmail.com>
2014-12-27 11:52:07 +0100

    add new activity, refactor

---
+++ src/com/example/RC_Car/AvailableDevicesListActivity.java
@@ -0,0 +1,16 @@
+package com.example.RC_Car;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Dawid Pawlak.
+ */
+public class AvailableDevicesListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.devices_list);
+    }
+}
---
+++ src/com/example/RC_Car/BluetoothActivity.java
@@ -0,0 +1,76 @@
+package com.example.RC_Car;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by Dawid Pawlak.
+ */
+public class BluetoothActivity extends Activity {
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    private Button connectButton;
+
+    private TextView bluetoothStatusView;
+
+    private BluetoothAdapter bluetoothAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        setUp();
+    }
+
+    private void setUp() {
+        initializeComponents();
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        setBluetoothStatus();
+    }
+
+    private void initializeComponents() {
+        connectButton = (Button) findViewById(R.id.connectBluetoothButton);
+        bluetoothStatusView = (TextView) findViewById(R.id.bluetoothStatusView);
+    }
+
+    public void connectBluetoothHandler(View view) {
+        if(bluetoothAdapter == null) {
+            bluetoothStatusView.setText(R.string.bluetoothDeviceNotFound);
+            finish();
+        }
+        Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBluetooth, REQUEST_ENABLE_BT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_ENABLE_BT) {
+            setBluetoothStatus();
+        }
+    }
+
+    private void setBluetoothStatus() {
+        if(bluetoothAdapter.isEnabled()) {
+            bluetoothStatusView.setText(R.string.bluetoothEnabled);
+            connectButton.setVisibility(View.GONE);
+        } else {
+            bluetoothStatusView.setText(R.string.bluetoothDisabled);
+        }
+    }
+
+    public void findDevicesHandler(View view) {
+        if(!bluetoothAdapter.isEnabled()) {
+            Toast.makeText(getApplicationContext(), R.string.bluetoothShouldBeEnabled, Toast.LENGTH_SHORT).show();
+        } else {
+            Intent intent = new Intent(this, AvailableDevicesListActivity.class);
+            startActivity(intent);
+        }
+    }
+}
